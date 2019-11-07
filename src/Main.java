78a46178dac3f0e30f3700321e79ad415ba560e2
Mike-Like11 <Lizasabrash2001@mail.ru>
2019-11-07 14:22:20 +0300

    hello?

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 
+    public static final PI = 3.1415;
     public static void main(String[] args) {
         System.out.println("Dofamin71");
         System.out.println("Dofamin71");
